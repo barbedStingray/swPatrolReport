2e55b510c81611538b1a15843beae78c284b80ec
Ben Rehmann <brehmann@me.com>
2023-09-02 13:00:04 -0500

    html edits

---
+++ client.js
@@ -0,0 +1,21 @@
+
+
+// imperial bases
+// endor shield generator, imperial center of research-scariff, spire-scariff, death star, cairn installation, coruscant shipyard
+// felucia imperial base, shipyard, detainment center
+// geonosis imperial base, shipyard, detainment center
+// imperial base mustafar, naboo, yavin 4, imperial mining base
+// imperial orbital base
+// outposts, dantooine, dathomir, endor, naboo, yavin 4, Deyer, Orra, Aurek-Two
+// imperial production laboratories
+// relay outposts
+// resource base, station alpha, beta, gamma, imperial secret facility
+// imperial weapons laboratory, weapons research facility weapons development facility
+// kashyyyk imperial base, 
+// naboo weapons depot
+// training camp imiif-138
+// talus imperial outpost
+
+// do you use a vehicle?
+
+
